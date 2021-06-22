6a9995cb8f5d9003a2888b55ba976f7f7eec25e6
jujupapaye <juliette.philibert@gmail.com>
2021-06-22 16:49:07 +0200

    Add files via upload

---
+++ ode_dfa_MNIST_test.py
@@ -0,0 +1,294 @@
+import argparse
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torchdiffeq import odeint
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--network', type=str, choices=['resnet', 'odenet'], default='odenet')
+parser.add_argument('--tol', type=float, default=1e-2)
+parser.add_argument('--adjoint', type=eval, default=False, choices=[True, False])
+parser.add_argument('--downsampling-method', type=str, default='conv', choices=['conv', 'res'])
+parser.add_argument('--nepochs', type=int, default=10)
+parser.add_argument('--data_aug', type=eval, default=True, choices=[True, False])
+parser.add_argument('--lr', type=float, default=0.001)
+parser.add_argument('--w_decay', type=float, default=0.1)
+parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--test_batch_size', type=int, default=1000)
+
+parser.add_argument('--gpu', type=int, default=1)
+args = parser.parse_args()
+
+MAX_NUM_STEPS = 200
+
+
+def to_categorical(y, num_classes):
+    return torch.eye(num_classes)[y]
+
+class Conv2dTime(nn.Conv2d):
+    """
+    Implements time dependent 2d convolutions, by appending the time variable as
+    an extra channel.
+    """
+    def __init__(self, in_channels, *args, **kwargs):
+        super(Conv2dTime, self).__init__(in_channels + 1, *args, **kwargs)
+
+    def forward(self, t, x):
+        # Shape (batch_size, 1, height, width)
+        t_img = torch.ones_like(x[:, :1, :, :]) * t
+        # Shape (batch_size, channels + 1, height, width)
+        t_and_x = torch.cat([t_img, x], 1)
+        return super(Conv2dTime, self).forward(t_and_x)
+
+
+class ConvODEFunc(nn.Module):
+    def __init__(self, device, img_size, num_filters, augment_dim=0,
+                 time_dependent=False, non_linearity='relu'):
+        super(ConvODEFunc, self).__init__()
+        self.device = device
+        self.augment_dim = augment_dim
+        self.img_size = img_size
+        self.time_dependent = time_dependent
+        self.nfe = 0  # Number of function evaluations
+        self.channels, self.height, self.width = img_size
+        self.channels += augment_dim
+        self.num_filters = num_filters
+        self.y_t = []
+
+        if time_dependent:
+            self.conv1 = Conv2dTime(self.channels, self.num_filters,
+                                    kernel_size=1, stride=1, padding=0)
+            self.conv2 = Conv2dTime(self.num_filters, self.num_filters,
+                                    kernel_size=3, stride=1, padding=1)
+            self.conv3 = Conv2dTime(self.num_filters, self.channels,
+                                    kernel_size=1, stride=1, padding=0)
+        else:
+            self.conv1 = nn.Conv2d(self.channels, self.num_filters,
+                                   kernel_size=1, stride=1, padding=0)
+            self.conv2 = nn.Conv2d(self.num_filters, self.num_filters,
+                                   kernel_size=3, stride=1, padding=1)
+            self.conv3 = nn.Conv2d(self.num_filters, self.channels,
+                                   kernel_size=1, stride=1, padding=0)
+
+        if non_linearity == 'relu':
+            self.non_linearity = nn.ReLU(inplace=True)
+        elif non_linearity == 'softplus':
+            self.non_linearity = nn.Softplus()
+        elif non_linearity =='lrelu':
+            self.non_linearity = torch.nn.LeakyReLU(negative_slope=0.01)
+
+    def forward(self, t, x):
+        self.nfe += 1
+        with torch.enable_grad():
+            out = x.detach()
+            out.requires_grad = True
+            if self.time_dependent:
+                out = self.conv1(t, x)
+                out = self.non_linearity(out)
+                out = self.conv2(t, out)
+                out = self.non_linearity(out)
+                out = self.conv3(t, out)
+            else:
+                out = self.conv1(x)
+                out = self.non_linearity(out)
+                out = self.conv2(out)
+                out = self.non_linearity(out)
+                out = self.conv3(out)
+            self.y_t.append(out)
+        return out
+
+
+class ODEBlock(nn.Module):
+    def __init__(self, device, odefunc, is_conv=False, tol=1e-3, adjoint=False):
+        super(ODEBlock, self).__init__()
+        self.adjoint = adjoint
+        self.device = device
+        self.is_conv = is_conv
+        self.odefunc = odefunc
+        self.tol = tol
+
+    def forward(self, x, eval_times=None):
+        self.odefunc.nfe = 0
+        self.odefunc.y_t = []
+        if eval_times is None:
+            integration_time = torch.tensor([0, 1]).float().type_as(x)
+        else:
+            integration_time = eval_times.type_as(x)
+
+        if self.odefunc.augment_dim > 0:
+            aug = torch.zeros(x.shape[0], self.odefunc.augment_dim).to(self.device)
+            x = torch.cat([x, aug], 1)
+
+
+        out = odeint(self.odefunc, x, integration_time,
+                         rtol=self.tol, atol=self.tol, method='dopri5',
+                         options={'max_num_steps': MAX_NUM_STEPS})
+
+        if eval_times is None:
+            return out[1]  # Return only final time
+        else:
+            return out
+
+    def trajectory(self, x, timesteps):
+        integration_time = torch.linspace(0., 1., timesteps)
+        return self.forward(x, eval_times=integration_time)
+
+
+
+class ConvODENet(nn.Module):
+    def __init__(self, device, img_size, num_filters, output_dim=1,
+                 augment_dim=0, time_dependent=False, non_linearity='relu',
+                 tol=1e-3, adjoint=False):
+        super(ConvODENet, self).__init__()
+        self.device = device
+        self.img_size = img_size
+        self.num_filters = num_filters
+        self.augment_dim = augment_dim
+        self.output_dim = output_dim
+        self.flattened_dim = (img_size[0] + augment_dim) * img_size[1] * img_size[2]
+        self.time_dependent = time_dependent
+        self.tol = tol
+
+        odefunc = ConvODEFunc(device, img_size, num_filters, augment_dim,
+                              time_dependent, non_linearity)
+
+        self.odeblock = ODEBlock(device, odefunc, is_conv=True, tol=tol,
+                                 adjoint=adjoint)
+
+        self.linear_layer = nn.Linear(self.flattened_dim, self.output_dim)
+
+    def forward(self, x, return_features=False):
+        features = self.odeblock(x)
+        pred = self.linear_layer(features.view(features.size(0), -1))
+        if return_features:
+            return features, pred
+        return pred
+
+
+def get_mnist_loaders(data_aug=False, batch_size=64, test_batch_size=1000, perc=1.0):
+    if data_aug:
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(28, padding=4),
+            transforms.ToTensor(),
+        ])
+    else:
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+        ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+    ])
+
+    train_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=True, download=False, transform=transform_train),
+        batch_size=batch_size,
+        shuffle=True, num_workers=2, drop_last=True
+    )
+
+    train_eval_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=True, download=False, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
+    )
+
+    test_loader = DataLoader(
+        datasets.MNIST(root='.data/mnist', train=False, download=False, transform=transform_test),
+        batch_size=test_batch_size, shuffle=False, num_workers=2, drop_last=True
+    )
+
+    return train_loader, test_loader, train_eval_loader
+
+if __name__ == '__main__':
+
+    img_size = (1, 28, 28)
+    output_dim = 10
+    augment_dim = 0
+
+    device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+
+    train_loader, test_loader, train_eval_loader = get_mnist_loaders(
+        args.data_aug, args.batch_size, args.test_batch_size
+    )
+
+    model = ConvODENet(device, img_size, num_filters=10,
+                       output_dim=output_dim,
+                       augment_dim=augment_dim,
+                       time_dependent=False,
+                       non_linearity='lrelu',
+                       adjoint=False)
+
+    model.to(device)
+
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=args.lr,
+                                 weight_decay=args.w_decay)
+
+    epoch_loss = 0.
+    epoch_nfes = 0
+    epoch_backward_nfes = 0
+    print_freq = 1
+    record_freq = 10
+    steps = 0
+    #histories = {'loss_history': [], 'nfe_history': [],
+                      #'bnfe_history': [], 'total_nfe_history': [],
+                      #'epoch_loss_history': [], 'epoch_nfe_history': [],
+                      #'epoch_bnfe_history': [], 'epoch_total_nfe_history': []}
+    #buffer = {'loss': [], 'nfe': [], 'bnfe': [], 'total_nfe': []}
+    loss_func = nn.CrossEntropyLoss(reduction='none').to(device) #nn.MSELoss() # nn.CrossEntropyLoss()
+    losses=[]
+    #torch.manual_seed(0)
+    random_mat = torch.randn((output_dim, 28*28)) / np.sqrt(output_dim*28*28)
+    #print(random_mat[0][0])
+    random_mat = random_mat.to(device)
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        optimizer.zero_grad()
+
+        x_batch = x_batch.to(device)
+        y_batch = y_batch.to(device)
+
+        y_pred = model(x_batch)
+        #print("y_pred", y_pred.size())
+
+        iteration_nfes = model.odeblock.odefunc.nfe
+        model.odeblock.odefunc.nfe = 0
+        epoch_nfes += iteration_nfes
+
+        error = y_pred - to_categorical(y_batch, output_dim).to(device)
+        nabla = torch.mm(error, random_mat)
+        nabla.to(device)
+        for y_i in model.odeblock.odefunc.y_t:
+            y_i = y_i.view(len(y_batch), -1)
+            y_i.backward(nabla, retain_graph=True)
+
+        loss = loss_func(y_pred, y_batch)
+        loss = loss.sum()
+        loss.backward(retain_graph=True)
+
+        optimizer.step()
+
+        iteration_backward_nfes = model.odeblock.odefunc.nfe
+        model.odeblock.odefunc.nfe = 0
+        epoch_backward_nfes += iteration_backward_nfes
+
+        print("\nIteration {}/{}".format(i, len(train_loader)))
+        print("Loss: ", loss.item())
+        losses.append(loss)
+
+        print("NFE: {}".format(iteration_nfes))
+        print("BNFE: {}".format(iteration_backward_nfes))
+        print("Total NFE: {}".format(iteration_nfes + iteration_backward_nfes))
+        steps += 1
+
+        print('Memory Usage:')
+        print('Allocated:', round(torch.cuda.memory_allocated(args.gpu)/1024**3,1), 'GB')
+        print('Cached:   ', round(torch.cuda.memory_cached(args.gpu)/1024**3,1), 'GB')
+
+        del x_batch
+        del y_batch
+        del error
+        del nabla
+
+
---
+++ ode_dfa_spiral_test.py
@@ -0,0 +1,252 @@
+import os
+import argparse
+import time
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn.functional import pad
+import matplotlib.pyplot as plt
+
+DIMENSION = 50
+
+parser = argparse.ArgumentParser('ODE demo')
+parser.add_argument('--method', type=str, choices=['dopri5', 'adams'], default='dopri5')
+parser.add_argument('--data_size', type=int, default=1000)
+parser.add_argument('--batch_time', type=int, default=10)
+parser.add_argument('--batch_size', type=int, default=60)
+parser.add_argument('--niters', type=int, default=300)
+parser.add_argument('--test_freq', type=int, default=50)
+parser.add_argument('--viz', action='store_true', default=True)
+parser.add_argument('--gpu', type=int, default=0)
+parser.add_argument('--adjoint', action='store_true', default=True)
+args = parser.parse_args()
+
+
+if args.adjoint:
+    from torchdiffeq import odeint_adjoint as odeint
+    from dfa import odeint_dfa, odeint_dfa2
+else:
+    from dfa import odeint_dfa, odeint_dfa2
+    from torchdiffeq import odeint
+
+#device = torch.device('cuda:' + str(args.gpu) if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu')
+true_y0 = torch.tensor([[2., 0.]]).to(device)
+t = torch.linspace(0., 25., args.data_size).to(device)
+true_A = torch.tensor([[-0.1, 2.0], [-2.0, -0.1]]).to(device)
+
+
+class Lambda(nn.Module):
+    def __init__(self):
+        super(Lambda, self).__init__()
+        self.y_t = []
+
+    def forward(self, t, y):
+        return torch.mm(y, true_A)
+
+
+with torch.no_grad():
+    true_y = odeint(Lambda(), true_y0, t, method='dopri5')
+
+
+def get_batch():
+    s = torch.from_numpy(
+        np.random.choice(np.arange(args.data_size - args.batch_time, dtype=np.int64), args.batch_size, replace=False))
+    batch_y0 = true_y[s]  # (M, D)
+    batch_t = t[:args.batch_time]  # (T)
+    batch_y = torch.stack([true_y[s + i] for i in range(args.batch_time)], dim=0)  # (T, M, D)
+    return batch_y0.to(device), batch_t.to(device), batch_y.to(device)
+
+
+def makedirs(dirname):
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+
+if args.viz:
+    makedirs('png')
+
+    fig = plt.figure(figsize=(12, 4), facecolor='white')
+    ax_traj = fig.add_subplot(131, frameon=False)
+    ax_phase = fig.add_subplot(132, frameon=False)
+    ax_vecfield = fig.add_subplot(133, frameon=False)
+    plt.show(block=False)
+
+
+def visualize(true_y, pred_y, odefunc, itr):
+    if args.viz:
+        ax_traj.cla()
+        ax_traj.set_title('Trajectories')
+        ax_traj.set_xlabel('t')
+        ax_traj.set_ylabel('x,y')
+        ax_traj.plot(t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 0], t.cpu().numpy(), true_y.cpu().numpy()[:, 0, 1],
+                     'g-')
+        ax_traj.plot(t.cpu().numpy(), pred_y.cpu().numpy()[:, 0, 0], '--', t.cpu().numpy(),
+                     pred_y.cpu().numpy()[:, 0, 1], 'b--')
+        ax_traj.set_xlim(t.cpu().min(), t.cpu().max())
+        ax_traj.set_ylim(-2, 2)
+        ax_traj.legend()
+
+        ax_phase.cla()
+        ax_phase.set_title('Phase Portrait')
+        ax_phase.set_xlabel('x')
+        ax_phase.set_ylabel('y')
+        ax_phase.plot(true_y.cpu().numpy()[:, 0, 0], true_y.cpu().numpy()[:, 0, 1], 'g-')
+        ax_phase.plot(pred_y.cpu().numpy()[:, 0, 0], pred_y.cpu().numpy()[:, 0, 1], 'b--')
+        ax_phase.set_xlim(-2, 2)
+        ax_phase.set_ylim(-2, 2)
+
+        ax_vecfield.cla()
+        ax_vecfield.set_title('Learned Vector Field')
+        ax_vecfield.set_xlabel('x')
+        ax_vecfield.set_ylabel('y')
+
+        y, x = np.mgrid[-2:2:21j, -2:2:21j]
+        dydt = odefunc(0, torch.Tensor(np.stack([x, y], -1).reshape(21 * 21, 2)).to(device)).cpu().detach().numpy()
+        mag = np.sqrt(dydt[:, 0] ** 2 + dydt[:, 1] ** 2).reshape(-1, 1)
+        dydt = (dydt / mag)
+        dydt = dydt.reshape(21, 21, 2)
+
+        ax_vecfield.streamplot(x, y, dydt[:, :, 0], dydt[:, :, 1], color="black")
+        ax_vecfield.set_xlim(-2, 2)
+        ax_vecfield.set_ylim(-2, 2)
+
+        fig.tight_layout()
+        plt.savefig('png/{:03d}'.format(itr))
+        plt.draw()
+        plt.pause(0.001)
+
+
+class ODEFunc(nn.Module):
+
+    def __init__(self):
+        super(ODEFunc, self).__init__()
+        self.y_t = []
+        self.tab_t = []
+        self.net = nn.Sequential(
+            nn.Linear(2, DIMENSION),
+            nn.Tanh(),
+            nn.Linear(DIMENSION, 2),
+        )
+
+        for m in self.net.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, mean=0, std=0.1)
+                nn.init.constant_(m.bias, val=0)
+
+    def forward(self, t, y):
+        with torch.enable_grad():
+            output = y.detach()
+            output.requires_grad = True
+            output = self.net(output)
+            self.y_t.append(output)
+            self.tab_t.append(t)
+        return output
+
+
+
+
+def main():
+    nb_expe = 10
+    test = 'dfa_ain'  # if 'dfa_main' : DFA back-prop else : adjoint method or auto-dif
+    losses_on_expe = []
+    good = 0
+    good_random = []
+    bad_random = []
+    nfe = []
+    times = []
+    losses = []
+    for i in range(nb_expe):
+        if test == 'dfa_main':  # backward en mode fda dans le main à la main
+            print("Expe ", i)
+            losses_on_expe.append([])
+            ii = 0
+            func = ODEFunc().to(device)
+
+            optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
+            begin = time.time()
+            output_size = 2
+
+            random_matrix = (torch.rand((output_size, output_size)) / np.sqrt(output_size)).to(device)
+            #random_matrix = torch.empty(output_size, output_size)
+            #torch.nn.init.orthogonal_(random_matrix)
+            #random_matrix = (random_matrix / np.sqrt(output_size)).to(device)
+
+            for itr in range(1, args.niters + 1):
+                optimizer.zero_grad()
+                batch_y0, batch_t, batch_y = get_batch()
+                batch_y0, batch_t, batch_y = batch_y0.to(device), batch_t.to(device), batch_y.to(device)
+                pred_y = odeint_dfa2(device, func, batch_y0, batch_t)
+                for tt in range(len(batch_t)):
+                    error = pred_y[tt] - batch_y[tt]
+                    nabla = torch.unsqueeze(torch.mm(error.squeeze(), random_matrix), dim=1)
+                    nabla.to(device)
+                    for j, y_i in enumerate(func.y_t):
+                        y_i.backward(nabla, retain_graph=True)
+                optimizer.step()
+                loss = torch.mean(torch.abs(pred_y - batch_y))
+
+                if itr % args.test_freq == 0:
+                    with torch.no_grad():
+                        y_plot = odeint_dfa2(device, func, true_y0, t)
+
+                        loss = torch.mean(torch.abs(y_plot - true_y))
+                        print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                        visualize(true_y, y_plot, func, ii)
+                        ii += 1
+                        losses_on_expe[i].append(loss)
+                if itr > 99 and loss > 100:
+                    print('nul')
+                    break
+                plt.cla()
+            end = time.time()
+            if loss < 0.2:
+                good += 1
+                good_random.append(random_matrix)
+            else:
+                bad_random.append(random_matrix)
+            print('loss :',loss.item())
+        else:  # normal nn with adjoint method
+            ii = 0
+
+            func = ODEFunc().to(device)
+
+            optimizer = optim.RMSprop(func.parameters(), lr=1e-3)
+            begin = time.time()
+
+            for itr in range(1, args.niters + 1):
+                optimizer.zero_grad()
+                batch_y0, batch_t, batch_y = get_batch()
+                pred_y = odeint(func, batch_y0, batch_t).to(device)
+                #nb_nfe = len(func.y_t)
+                #func.y_t = []
+                loss = torch.mean(torch.abs(pred_y - batch_y))
+                loss.backward()
+                optimizer.step()
+
+                if itr % args.test_freq == 0:
+                    with torch.no_grad():
+                        pred_y = odeint(func, true_y0, t)
+                        loss = torch.mean(torch.abs(pred_y - true_y))
+                        print('Iter {:04d} | Total Loss {:.6f}'.format(itr, loss.item()))
+                        visualize(true_y, pred_y, func, ii)
+                        ii += 1
+
+
+        end = time.time()
+        #nfe.append(nb_nfe)
+        times.append(end-begin)
+        if loss.item() < 1:
+            losses.append(loss.item())
+        print('time:', end-begin)
+    #print('dfa')
+    print('MEAN TIMES',np.mean(times))
+    print('MEAN LOSS',np.mean(losses))
+    return good, good_random, bad_random, nfe
+
+
+if __name__ == '__main__':
+    good, good_random, bad_random,nfe = main()
+    print('good game', good)
